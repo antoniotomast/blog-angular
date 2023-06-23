cee5d42a18bf134800d60baf3223ddfa2fc35589
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-06-23 19:13:33 +0200

    Creadas rutas de escape en el routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { ListaPostsComponent } from './components/lista-posts/lista-posts.compon
 import { FormularioComponent } from './components/formulario/formulario.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/posts' },
   { path: 'posts', component: ListaPostsComponent },
-  { path: 'blog/new', component: FormularioComponent }
+  { path: 'blog/new', component: FormularioComponent },
+  { path: '**', redirectTo: '/posts' }
 ];
 
 @NgModule({
